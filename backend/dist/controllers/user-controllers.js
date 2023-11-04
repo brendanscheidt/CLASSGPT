19fa1331ad76384b48f0134f94b5f004b66e4083
Brendan Scheidt <brendanscheidt@gmail.com>
2023-11-03 21:54:52 -0600

    Add POST req for user signup

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,10 +1,24 @@
 import User from "../models/User.js";
+import { hash } from "bcrypt";
 export const getAllUsers = async (req, res, next) => {
     //get all users
     try {
         const users = await User.find();
-        console.log("hit");
         return res.status(200).json({ message: "OK", users });
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(404).json({ message: "ERROR", cause: err.message });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    //user signup
+    try {
+        const { name, email, password } = req.body;
+        const hashedPass = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPass });
+        await user.save();
+        return res.status(200).json({ message: "OK", id: user._id.toString() });
     }
     catch (err) {
         console.log(err);
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
+import { hash } from "bcrypt";
 
 export const getAllUsers = async (
   req: Request,
@@ -9,9 +10,29 @@ export const getAllUsers = async (
   //get all users
   try {
     const users = await User.find();
-    console.log("hit");
 
     return res.status(200).json({ message: "OK", users });
+  } catch (err) {
+    console.log(err);
+
+    return res.status(404).json({ message: "ERROR", cause: err.message });
+  }
+};
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //user signup
+  try {
+    const { name, email, password } = req.body;
+    const hashedPass = await hash(password, 10);
+    const user = new User({ name, email, password: hashedPass });
+
+    await user.save();
+
+    return res.status(200).json({ message: "OK", id: user._id.toString() });
   } catch (err) {
     console.log(err);
 
