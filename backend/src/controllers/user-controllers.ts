35e3cfd77200fe601bdd2714ce2f85042ea195d5
Brendan Scheidt <brendanscheidt@gmail.com>
2023-11-03 22:49:33 -0600

    Add controller for userLogin and loginValidator array of validations for validate middleware. new userRoute added with this route

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 
 export const getAllUsers = async (
   req: Request,
@@ -27,12 +27,42 @@ export const userSignup = async (
   //user signup
   try {
     const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) return res.status(401).send("User already registered");
+
     const hashedPass = await hash(password, 10);
     const user = new User({ name, email, password: hashedPass });
 
     await user.save();
 
     return res.status(201).json({ message: "OK", id: user._id.toString() });
+  } catch (err) {
+    console.log(err);
+
+    return res.status(404).json({ message: "ERROR", cause: err.message });
+  }
+};
+
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //user login
+  try {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) return res.status(401).send("User not registered");
+
+    const isPasswordCorrect = await compare(password, existingUser.password);
+
+    if (!isPasswordCorrect) return res.status(403).send("Incorrect Password");
+
+    return res
+      .status(201)
+      .json({ message: "OK", id: existingUser._id.toString() });
   } catch (err) {
     console.log(err);
 
