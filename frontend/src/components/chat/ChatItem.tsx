f71c7e2285339b0f4fa0b1685275d719e9e1686d
Brendan Scheidt <brendanscheidt@gmail.com>
2023-11-11 15:01:48 -0700

    Add code syntax display
    - Added the ability for ChatItem component to detect md code snippets in
      Openai response
    - Grab the language from the code snippet and display the snippet as
      code in response with react-syntax-highlighter

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -1,5 +1,30 @@
-import { Avatar, Box, Typography } from "@mui/material";
+import { Box, Avatar, Typography } from "@mui/material";
 import { useAuth } from "../../context/AuthContext";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+function extractCodeFromString(message: string) {
+  if (message.includes("```")) {
+    const blocks = message.split("```");
+    return blocks;
+  }
+}
+
+function isCodeBlock(str: string) {
+  if (
+    str.includes("=") ||
+    str.includes(";") ||
+    str.includes("[") ||
+    str.includes("]") ||
+    str.includes("{") ||
+    str.includes("}") ||
+    str.includes("#") ||
+    str.includes("//")
+  ) {
+    return [true, str.trim().split("\n")[0].trim()];
+  }
+  return [false, ""];
+}
 
 const ChatItem = ({
   content,
@@ -8,28 +33,82 @@ const ChatItem = ({
   content: string;
   role: "user" | "assistant";
 }) => {
+  const messageBlocks = extractCodeFromString(content);
   const auth = useAuth();
-
-  return role === "assistant" ? (
-    <Box sx={{ display: "flex", p: 2, bgcolor: "#004d5612", my: 2, gap: 2 }}>
+  return role == "assistant" ? (
+    <Box
+      sx={{
+        display: "flex",
+        p: 2,
+        bgcolor: "#004d5612",
+        gap: 2,
+        borderRadius: 2,
+        my: 1,
+      }}
+    >
       <Avatar sx={{ ml: "0" }}>
         <img src="openai.png" alt="openai" width={"30px"} />
       </Avatar>
       <Box>
-        <Typography fontSize={"20px"}>{content}</Typography>
+        {!messageBlocks && (
+          <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
+        )}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((block, index) => {
+            const [isCode, lang] = isCodeBlock(block);
+            return isCode && typeof lang === "string" ? (
+              <SyntaxHighlighter
+                key={index}
+                style={coldarkDark}
+                language={lang.toLowerCase()}
+              >
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography key={index} sx={{ fontSize: "20px" }}>
+                {block}
+              </Typography>
+            );
+          })}
       </Box>
     </Box>
   ) : (
-    <Box sx={{ display: "flex", p: 2, bgcolor: "#004d56", gap: 2 }}>
+    <Box
+      sx={{
+        display: "flex",
+        p: 2,
+        bgcolor: "#004d56",
+        gap: 2,
+        borderRadius: 2,
+      }}
+    >
       <Avatar sx={{ ml: "0", bgcolor: "black", color: "white" }}>
-        {/* first letter of first and last name */}
         {auth?.user?.name[0]}
-        {auth?.user?.name.split(" ")[1]
-          ? auth?.user?.name.split(" ")[1][0]
-          : ""}
+        {auth?.user?.name.split(" ")[1][0]}
       </Avatar>
       <Box>
-        <Typography fontSize={"20px"}>{content}</Typography>
+        {!messageBlocks && (
+          <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
+        )}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((block, index) => {
+            const [isCode, lang] = isCodeBlock(block);
+            return isCode && typeof lang === "string" ? (
+              <SyntaxHighlighter
+                key={index}
+                style={coldarkDark}
+                language={lang.toLowerCase()}
+              >
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography key={index} sx={{ fontSize: "20px" }}>
+                {block}
+              </Typography>
+            );
+          })}
       </Box>
     </Box>
   );
