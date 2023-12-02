93a31a257262b6030129aacb4fe9b583d23aed7f
Brendan Scheidt <brendanscheidt@gmail.com>
2023-12-02 14:31:45 -0700

    heroku commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ config();
 const app = express();
 
 // Middleware //
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.json());
 
