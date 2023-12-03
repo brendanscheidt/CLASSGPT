8ad31f9d00120614e5a952c5ce811792f8d4d950
Brendan Scheidt <brendanscheidt@gmail.com>
2023-12-03 13:42:02 -0700

    mongo connection

--- backend/dist/worker/JobQueue.js
+++ backend/dist/worker/JobQueue.js
@@ -1,6 +1,17 @@
 import Queue from "bull";
 import dotenv from "dotenv";
+import { connect } from "mongoose";
 dotenv.config();
+async function connectToDatabase() {
+    try {
+        await connect(process.env.MONGODB_URL);
+    }
+    catch (err) {
+        console.log(err);
+        throw new Error("Cannot Connect to MongoDB");
+    }
+}
+connectToDatabase();
 const port = parseInt(process.env.REDIS_PORT, 10);
 if (isNaN(port)) {
     throw new Error("Invalid Redis port number");
