881e6328cc90cd7f98229de486ae0277ba691e0b
Brendan Scheidt <brendanscheidt@gmail.com>
2023-12-04 20:09:51 -0700

    fix width issues

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -95,7 +95,7 @@ const Login = () => {
                 px: 2,
                 py: 1,
                 mt: 2,
-                width: "400px",
+                width: "100%",
                 borderRadius: 2,
                 bgcolor: "#00fffc",
                 ":hover": {
